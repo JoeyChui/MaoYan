4ca36686aa597ce91d501e7252613d7c9caee472
Joseph Cui <joeychui@qq.com>
2017-08-13 12:48:33 +0800

    my CrawlMaoYan

--- CrawlMaoYan.py
+++ CrawlMaoYan.py
@@ -1,10 +1,9 @@
-import json
-from multiprocessing import Pool
-import requests
-from requests.exceptions import RequestException
-import re
 
-def get_one_page(url):
+import re, json, requests
+from requests.exceptions import RequestException
+from multiprocessing import Pool
+
+def getOnePage(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -13,32 +12,31 @@ def get_one_page(url):
     except RequestException:
         return None
 
-def parse_one_page(html):
+def parseOnePage(html):
     pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
                          +'.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
                          +'.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
     items = re.findall(pattern, html)
     for item in items:
         yield {
-            'index': item[0],
-            'image': item[1],
-            'title': item[2],
-            'actor': item[3].strip()[3:],
-            'time': item[4].strip()[5:],
-            'score': item[5]+item[6]
-        }
+                'index': item[0],
+                'image': item[1],
+                'title': item[2],
+                'actor': item[3].strip()[3:],
+                'time': item[4].strip()[5:],
+                'score': item[5]+item[6]
+              }
 
-def write_to_file(content):
+def writeToFile(content):
     with open('result.txt', 'a', encoding='utf-8') as f:
         f.write(json.dumps(content, ensure_ascii=False) + '\n')
         f.close()
 
 def main(offset):
     url = 'http://maoyan.com/board/4?offset=' + str(offset)
-    html = get_one_page(url)
-    for item in parse_one_page(html):
-        print(item)
-        write_to_file(item)
+    html = getOnePage(url)
+    for item in parseOnePage(html):
+        writeToFile(item)
 
 
 if __name__ == '__main__':
